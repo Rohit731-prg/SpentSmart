1c323735911fb548a5be1e9937aa36ec2ea31cca
Rohit731-prg <rohitsingha821380@gmail.com>
2026-09-18 14:28:30 +0530

    fix url miss-match

--- Server/app/main.py
+++ Server/app/main.py
@@ -15,7 +15,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:3000",
-        setting.CLOUD_BACKEND_URL
+        setting.CLOUD_BACKEND_URL,
+        "https://spentsmart-client.onrender.com/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
